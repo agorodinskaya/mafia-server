7450c9b373f5c7d71b4d3f27c8e38f3e57ad228a
agorodinskaya <agorodinskaya@gmail.com>
2018-06-14 15:39:39 +1000

    added redis to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.url = 'https://mafiaserverwdi27.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins =['https://mafiaserverwdi27.herokuapp.com']
 end
